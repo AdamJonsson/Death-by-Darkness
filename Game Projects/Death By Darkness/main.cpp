9f0ff2e1e27bfcf1c62ecbdc5fc09390bfceecaf
Edvin Åkerstrom <edvin.akerstrom@hotmail.se>
2015-08-27 21:40:08 +0200

    Revert "G"
    This reverts commit c295af6d7bfcf4329b5cf959cc67d88972918371.

--- Game Projects/Death By Darkness/main.cpp
+++ Game Projects/Death By Darkness/main.cpp
@@ -2,8 +2,34 @@
 #include <time.h>
 #include <chrono>
 
-int main() {
+bool running;
+int up = 0;
 
+int main() {
+	auto lastTime = std::chrono::high_resolution_clock::now();
+	long long timer = clock();
+	const double ns = 1000000000.0 / 60.0;
+	double delta = 0.0;
+	int frames = 0;
+	int updates = 0;
+	while (running) {
+		long long now = clock();
+		delta += (now - lastTime) / ns;
+		lastTime = now;
+		while (delta >= 1) {
+			update();
+			updates++;
+			delta--;
+		}
+		render();
+		frames++;
+
+		if (clock() - timer > 1000) {
+			timer += 1000;
+			updates = 0;
+			frames = 0;
+		}
+	}
 }
 
 void update() {
@@ -11,5 +37,6 @@ void update() {
 }
 
 void render() {
-
+	std::cout << "Update number: " << up << std::endl;
+	up++;
 }
